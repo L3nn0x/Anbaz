f3d933c8264bdeb036de8951fbb4a181db82a67b
l3nn0x <dragon83.super@gmail.com>
2024-04-21 17:09:49 +0200

    Add ast pretty print

--- frontend/__main__.py
+++ frontend/__main__.py
@@ -9,6 +9,7 @@ from .ir import GenerateIr
 parser = argparse.ArgumentParser(description='Description of your program')
 parser.add_argument('input_file', type=str, help='Input file path')
 parser.add_argument('-o', '--output_file', type=str, help='Output file path')
+parser.add_argument('-p', '--print_ast', action='store_true', help='prints the ast then exits')
 args = parser.parse_args()
 
 input_file = args.input_file
@@ -17,9 +18,34 @@ output_file = args.output_file
 if not output_file:
     output_file = input_file + ".s"
 
+class PrintVisitor:
+    def __init__(self):
+        self.current_type = None
+        self.current_expr = None
+        self.body = ""
+
+    def visit_type(self, t):
+        self.current_type = t.t
+
+    def visit_const(self, c):
+        self.current_expr = f"const_{self.current_type}({c.value})"
+
+    def visit_return(self, r):
+        self.body += f"\treturn {'' if not self.current_expr else self.current_expr}\n"
+        self.current_expr = None
+
+    def visit_function(self, f):
+        print(f"function {self.current_type} {f.name}:")
+        print(f"args: {f.args}")
+        print(self.body)
+
 contents = read_file(input_file)
 tokens = tokenize(contents)
 ast = parse_translation_unit(Peekable(iter(tokens)))
-irv = GenerateIr()
-ast.visit(irv)
-print(json.dumps(irv.generate()))
+if args.print_ast:
+    p = PrintVisitor()
+    ast.visit(p)
+else:
+    irv = GenerateIr()
+    ast.visit(irv)
+    print(json.dumps(irv.generate()))
